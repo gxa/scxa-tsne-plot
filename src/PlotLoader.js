68b4f88267c07aed1e488390cff84348cc721f34
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-20 14:07:39 +0100

    Add new props to set height or dim/highlight series; enrich state of the component with a loading boolean and an error object; display LoadingOverlay or an error message based on these two fields

--- src/PlotLoader.js
+++ src/PlotLoader.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import URI from 'urijs'
 
+import LoadingOverlay from './LoadingOverlay'
 import ScatterPlot from './ScatterPlot'
 
 class PlotLoader extends React.Component {
@@ -9,53 +10,66 @@ class PlotLoader extends React.Component {
     super(props)
 
     this.state = {
-      timestamp: 0,
+      // timestamp: 0,
       series: [],
+      loading: false,
       error: null
     }
   }
 
-  async _fetchAndUpdate(base, endpoint) {
+  async _fetchAndSetState(base, endpoint) {
     try {
+      this.setState({
+        loading: true
+      })
       const response = await fetch(URI(endpoint, base).toString())
-      this.setState(
-        {
-          timestamp: Date.now(),
-          series: await response.json()
-        },
-        this.forceUpdate)
+      this.setState({
+        // timestamp: Date.now(),
+        series: await response.json(),
+        loading: false,
+        error: null
+      })
     }
     catch (err) {
-      console.log(`Fetch failed`, err)
+      this.setState({
+        // timestamp: Date.now(),
+        error: err,
+        loading: false
+      })
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.sourceUrl !== nextProps.sourceUrl || this.props.atlasUrl !== nextProps.atlasUrl) {
-      this._fetchAndUpdate(nextProps.atlasUrl, nextProps.sourceUrl)
+    if (nextProps.atlasUrl !== this.props.atlasUrl || nextProps.sourceUrl !== this.props.sourceUrl) {
+      this._fetchAndSetState(nextProps.atlasUrl, nextProps.sourceUrl)
     }
   }
 
-  shouldComponentUpdate() {
-    return false
-  }
-
   componentDidMount() {
-    this._fetchAndUpdate(this.props.atlasUrl, this.props.sourceUrl)
+    this._fetchAndSetState(this.props.atlasUrl, this.props.sourceUrl)
   }
 
   render() {
-    return (
-      this.state.timestamp ?
-        <ScatterPlot series={this.state.series}/> :
-        <div><img src={URI(`resources/images/loading.gif`, this.props.atlasUrl).toString()}/></div>
+    const {height, highlightSeries} = this.props
+    const {series, loading, error} = this.state
+
+    return(
+      error ?
+        <div style={{textAlign: `center`}}><p>Error: {error.toString()}</p></div> :
+
+        <div style={{position: `relative`}}>
+          <ScatterPlot series={series} highlightSeries={highlightSeries} height={height}/>
+          <LoadingOverlay show={loading}/>
+        </div>
     )
   }
 }
 
 PlotLoader.propTypes = {
   atlasUrl: PropTypes.string.isRequired,
-  sourceUrl: PropTypes.string.isRequired
+  sourceUrl: PropTypes.string.isRequired,
+  highlightSeries: PropTypes.array,
+  height: PropTypes.number
 }
 
 export default PlotLoader
