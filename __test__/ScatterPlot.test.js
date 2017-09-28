19d94cbc5480dd3c6009a4f7773b661d05304e32
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-28 17:43:13 +0100

    Add tests and snapshots for ScatterPlot component

---
+++ __test__/ScatterPlot.test.js
@@ -0,0 +1,64 @@
+import React from 'react'
+// Highcharts can only be shallow-rendered unless it’s mocked, see __mocks__/highcharts.js
+import Enzyme from 'enzyme'
+import {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import HighchartsSeriesGenerator from 'highcharts-series-generator'
+
+import ScatterPlot from '../src/ScatterPlot'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe(`ScatterPlot`, () => {
+
+  test(`merges additional options for Highcharts`, () => {
+    
+  })
+
+  test(`with no series matches snapshot`, () => {
+    const tree = shallow(<ScatterPlot series={[]}/>)
+    expect(tree).toMatchSnapshot()
+  })
+
+  test(`matches snapshot with randomized series`, () => {
+    const seed = `A hair, Morty. I need one of your hairs. This isn’t Game of Thrones.`
+    const seriesNames = [`Series 1`, `Series 2`, `Series 3`, `Series 4`, `Series 5`]
+    const maxPointsPerSeries = 1000
+    const series = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries, seed)
+    const tree = shallow(<ScatterPlot series={series}/>)
+    expect(tree).toMatchSnapshot()
+  })
+
+
+  test(`doesn’t check for unique names in the same series or among different series`, () => {
+    const series = {
+      series: [
+        {
+          name: `Cluster 1`,
+          data: [
+            {x: 0, y: 0, name: `Cell A`, expressionLevel: 0},
+            {x: 0, y: 0, name: `Cell A`, expressionLevel: 0},
+            {x: 1.11111, y: 2.22222, name: `Cell B`, expressionLevel: 9.99999}
+          ]
+        },
+        {
+          name: `Cluster 2`,
+          data: [
+            {x: 0, y: 0, name: `Cell A`, expressionLevel: 0},
+            {x: 1.11111, y: 2.22222, name: `Cell B`, expressionLevel: 9.99999}
+          ]
+        },
+        {
+          name: `Cluster 3`,
+          data: [
+            {x: 1.11111, y: 2.22222, name: `Cell A`, expressionLevel: 9.99999}
+          ]
+        }
+      ]
+    }
+
+    const tree = shallow(<ScatterPlot {...series}/>)
+    expect(tree).toMatchSnapshot()
+  })
+})
