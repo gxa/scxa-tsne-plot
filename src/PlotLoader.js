9ba96d8cc680937afb4e59cf65c7afbe9b8fe335
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-28 15:50:35 +0100

    Do async fetch right; add error boundary

--- src/PlotLoader.js
+++ src/PlotLoader.js
@@ -5,6 +5,12 @@ import URI from 'urijs'
 import LoadingOverlay from './LoadingOverlay'
 import ScatterPlot from './ScatterPlot'
 
+const _fetch = async (base, endpoint) => {
+  const response = await fetch(URI(endpoint, base).toString())
+  const responseJson = await response.json()
+  return responseJson
+}
+
 class PlotLoader extends React.Component {
   constructor(props) {
     super(props)
@@ -13,55 +19,61 @@ class PlotLoader extends React.Component {
       // timestamp: 0,
       series: [],
       loading: false,
-      error: null
+      errorMessage: null
     }
-  }
-
-  async _fetchAndSetState(base, endpoint) {
-    try {
-      this.setState({
-        loading: true
-      })
-      const response = await fetch(URI(endpoint, base).toString())
-      this.setState({
-        // timestamp: Date.now(),
-        series: await response.json(),
-        loading: false,
-        error: null
-      })
-    }
-    catch (err) {
-      this.setState({
-        // timestamp: Date.now(),
-        error: err,
-        loading: false
-      })
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.atlasUrl !== this.props.atlasUrl || nextProps.sourceUrl !== this.props.sourceUrl) {
-      this._fetchAndSetState(nextProps.atlasUrl, nextProps.sourceUrl)
-    }
-  }
-
-  componentDidMount() {
-    this._fetchAndSetState(this.props.atlasUrl, this.props.sourceUrl)
   }
 
   render() {
     const {resourcesUrl, seriesMapper, highchartsConfig} = this.props
-    const {series, loading, error} = this.state
+    const {series, loading, errorMessage} = this.state
 
     return(
-      error ?
-        <div style={{textAlign: `center`}}><p>Error: {error.toString()}</p></div> :
+      errorMessage ?
+        <div className={`scxa-error`} style={{textAlign: `center`}}><p>{errorMessage}</p></div> :
 
         <div style={{position: `relative`}}>
           <ScatterPlot series={seriesMapper(series)} highchartsConfig={highchartsConfig}/>
           <LoadingOverlay show={loading} resourcesUrl={resourcesUrl}/>
         </div>
     )
+  }
+
+  _fetchAndSetState() {
+    this.setState({
+      loading: true
+    })
+    return _fetch(this.props.atlasUrl, this.props.sourceUrl)
+      .then((responseJson) => {
+        this.setState({
+          // timestamp: Date.now(),
+          series: responseJson,
+          loading: false,
+          errorMessage: null
+        })
+      })
+      .catch((reason) => {
+        this.setState({
+          // timestamp: Date.now(),
+          errorMessage: `${reason.name}: ${reason.message}`,
+          loading: false
+        })
+      })
+  }
+
+  componentDidMount() {
+    return this._fetchAndSetState()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.atlasUrl !== this.props.atlasUrl || nextProps.sourceUrl !== this.props.sourceUrl) {
+      return this._fetchAndSetState()
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+       errorMessage: `${error}`
+     })
   }
 }
 
